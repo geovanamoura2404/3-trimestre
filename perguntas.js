1e5a84865d67e18f8b7af60f8de72e302c4a12db
geovanamoura2404 <geovana.santos.moura@escola.pr.gov.br>
2026-09-14 10:54:43 -0300

    iyfyfcyfv

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criarCartao(
+    'Categoria',
+    'Perginta',
+    'Resposta'
+)
+ criarCartao(
+    'Geografia',
+    'Qual é o maior país da América do Sul?',
+    'é o Brasil'
+)
+criarCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'Brasília'
+)
+criarCartao(
+    'Geografia ',
+    'Em qual continente fica o Brasil?',
+    'Ame'
+)
